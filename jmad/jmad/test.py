188773f20afc9a134e027b1f19473f1c30848e7b
aragakerubo <aragakerubo@gmail.com>
2021-04-09 13:37:56 +0300

    Extends functional test for model and URL refactor

--- jmad/jmad/test.py
+++ jmad/jmad/test.py
@@ -83,23 +83,29 @@ class StudentTestCase(LiveServerTestCase):
         # The solo page has the title, artist and album for
         # this particular solo.
         self.assertEqual(
-            self.browser.current_url, "{}/solos/2/".format(self.live_server_url)
+            self.browser.current_url, self.live_server_url + "/recordings/kind-of-blue/all-blues/cannonball-adderley/"
         )
+
+        # he sees the artist ...
         self.assertEqual(
             self.browser.find_element_by_css_selector("#jmad-artist").text, "Cannonball Adderley"
         )
+
+        # the track title (with count of solos) ...
         self.assertEqual(
-            self.browser.find_element_by_css_selector("#jmad-track").text, "All Blues"
+            self.browser.find_element_by_css_selector("#jmad-track").text, "All Blues [2 solos]"
         )
+
+        # and the album title (wit track count) for this solo
         self.assertEqual(
-            self.browser.find_element_by_css_selector("#jmad-album").text, "Kind of Blue"
+            self.browser.find_element_by_css_selector("#jmad-album").text, "Kind of Blue [3 tracks]"
         )
 
         # He also sees the start and the end time of the
         # solo
-        self.assertEqual(
-            self.browser.find_element_by_css_selector("#jmad-start-time").text, "2:06"
-        )
-        self.assertEqual(
-            self.browser.find_element_by_css_selector("#jmad-end-time").text, "4:01"
-        )
+        # self.assertEqual(
+        #     self.browser.find_element_by_css_selector("#jmad-start-time").text, "2:06"
+        # )
+        # self.assertEqual(
+        #     self.browser.find_element_by_css_selector("#jmad-end-time").text, "4:01"
+        # )
